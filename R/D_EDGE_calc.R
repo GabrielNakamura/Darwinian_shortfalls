2c71eb74da693a2c3d82ae82e3b815a7de0ab127
GabrielNakamura <gabriel.nakamura.souza@gmail.com>
2022-09-10 21:28:08 -0500

    adding EDGE calculation

---
+++ R/D_EDGE_calc.R
@@ -0,0 +1,25 @@
+
+# data and library --------------------------------------------------------
+
+library(phyloregion)
+spp_df <- read.table(here::here("data", "taxa_table.txt"), header = TRUE) # species cleaned
+res_phylo_marine <- readRDS(file = here::here("output", "phylo_marine.rds"))
+
+phylo_marine <- res_phylo_marine$Phylogeny
+db_fishbase <- species() 
+
+
+
+# cleaning data -----------------------------------------------------------
+
+cons_status <- db_fishbase$Vulnerability # check if this can be used as a proxy for IUCN endangered categories
+cons_status_spp_fb <- db_fishbase[which(is.na(cons_status) != TRUE), ]
+status_df_spp <- cons_status_spp_fb[which(is.na(match(spp_df$s, unique(gsub(" ", "_", cons_status_spp_fb$Species)))) != TRUE), "Vulnerability"]
+
+# calculate EDGE ----------------------------------------------------------
+
+data(africa)
+y <- EDGE(x=africa$IUCN, phy=africa$phylo, Redlist="IUCN", species="Species")
+?evol_distinct
+?EDGE
+install.packages("rredlist")
