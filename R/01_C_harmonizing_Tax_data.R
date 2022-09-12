b47b9a8095a61e8d32624dafbba8b817670223d6
GabrielNakamura <gabriel.nakamura.souza@gmail.com>
2022-09-12 18:29:17 -0500

    checking names

--- R/01_C_harmonizing_Tax_data.R
+++ R/01_C_harmonizing_Tax_data.R
@@ -50,4 +50,15 @@ df_fishbase <- data.frame(spp_FB = db_fishbase$Species, code = db_fishbase$Speci
 
 # harmonizing with taxadb -------------------------------------------------
 
-joined_esch_fb <- full_join(df_esch, df_fishbase, by = c("spp_esch" = "spp_FB"))
+joined_esch_fb <- full_join(df_esch, df_fishbase, by = c("spp_esch" = "spp_FB")) # just joining all names 
+
+# now getting IDs from Catalog of Life 
+esch <- df_esch %>% mutate(id = get_ids(spp_esch, "col"))
+
+fishbase <- df_fishbase %>% mutate(id = get_ids(spp_FB, "col"))
+esch_code <- esch[-which(is.na(esch$id) == TRUE), ]
+fb_code <- fishbase[-which(is.na(fishbase$id) == TRUE), ]
+joined_esch_fb <- full_join(esch_code[, c(1,3)], fb_code[, c(1,3)], by = c("id")) # most reliable level of similarity btween datasets
+
+joined_esch_fb <- full_join(esch[, c(1,3)], fishbase[, c(1,3)], by = c("id")) # just joining all names 
+dim(joined_esch_fb)
