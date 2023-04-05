3173ee578c11379d83612f0ccbdf55d2af1ea9ae
GabrielNakamura <gabriel.nakamura.souza@gmail.com>
2023-04-05 12:00:32 -0300

    updating phylogenetic tree and grafting table

--- R/D_Order_grafting.R
+++ R/D_Order_grafting.R
@@ -10,13 +10,13 @@ species <- data$s # only species names
 
 
 
+# grafting species up to family level -------------------------------------
+
 # getting the tree up to family level using rtrees 
 sp_tree <- rtrees::get_tree(sp_list = species, taxon = 'fish', scenario = 'at_basal_node') # phylo object
 tb_tree <- as_tibble(sp_tree) # tibble obj
 
 tb_graft <- sp_tree$graft_status # status of insertion
-
-
 
 tb_tree_graft <- 
   dplyr::full_join(tb_tree, tb_graft, by = "label") %>% 
@@ -24,39 +24,42 @@ tb_tree_graft <-
 no_family <- tb_tree_graft@data[which(tb_tree_graft@data$status == "skipped as no co-family in the megatree"), ] # no species in megatree
 
 
-# adding species at order level
+# getting species and nodes to be added at order level --------------------
 
 tax <- fishtree::fishtree_taxonomy() # taxonomy from rfishtree
 orders <-  tax[which(tax$rank == "order"), ]
 orders_no_incertae <- orders[-grep("Incertae sedis in", orders$name), ] # removing incertae sedis
+
+# getting species names for all species at order level
 spp_order <- lapply(orders_no_incertae$name, function(x) fishtree::fishtree_taxonomy(ranks = x)[[1]]$sampled_species)
 names(spp_order) <- orders_no_incertae$name
 spp_order <- lapply(spp_order, function(x) gsub(" ", "_", x))
 
-# all node position
+# getting all node position for orders
 mrca_order_node <- lapply(spp_order, function(x) tidytree::MRCA(tb_tree, c(tb_tree[na.omit(match(x, tb_tree$label)), "label"])[[1]])) 
 
-# species with no co-family
+# species with no co-family in phylogenetic tree
 data_not_family <- data[match(no_family$species, data$s), ]
 data_not_family <- data_not_family[-which(data_not_family$o == "Incertae sedis in Eupercaria"), ]
+
+# getting the nodes for each order of species to be inserted
 data_order <- mrca_order_node[na.omit(match(names(table(data_not_family$o)), names(mrca_order_node)))]
 names(data_order) # species with orders in the tree
-
-# adding species to order node number
-data_order_node <- lapply(data_order[na.omit(match(data_not_family$o, names(data_order)))], function(x) x$node)
+data_order_node <- lapply(data_order[na.omit(match(data_not_family$o, names(data_order)))], function(x) x$node) # nodes of each order
 order_node_info <- data_not_family[which(!is.na(match(data_not_family$o, names(data_order))) == TRUE), ]
 order_node_info$OrdNode <- unlist(data_order_node)
-order_node_info$NameCheckOrd <- names(unlist(data_order_node))
+order_node_info$NameCheckOrd <- names(unlist(data_order_node)) # data frame with all nodes of orders for all species to be grafted
 
+# grafting species --------------------------------------------------------
+
+tree_update_order <- as.phylo(tb_tree)
+tree_update_order <- phytools::force.ultrametric(tree_update_order, method = "extend")
+
+# just a bar progress
 pb_order <- progress::progress_bar$new(format = "Adding species to order nodes [:bar] :percent", 
                                        total = dim(order_node_info)[1], clear = FALSE, 
                                        width = 60, current = "<", incomplete = ">", 
                                        complete = ">")
-
-tree_update_order <- as.phylo(tb_tree)
-is.ultrametric(tree_update_order)
-tree_update_order <- force.ultrametric(tree_update_order, method = "extend")
-
 
 # binding species to order node
 for(i in 1:dim(order_node_info)[1]){
@@ -69,8 +72,15 @@ for(i in 1:dim(order_node_info)[1]){
 }
 
 
-# saving trees
+
+# insertion table ---------------------------------------------------------
+
+pos <- match(order_node_info$s, tb_graft$species)
+tb_graft[pos, "status"] <- "grafted at order level"
+
+# saving trees and insertion table ----------------------------------------
 
 saveRDS(object = as.phylo(tb_tree), here::here("output", "family_tree.rds")) 
 saveRDS(object = tree_update_order, here::here("output", "order_tree.rds"))
+saveRDS(object = tb_graft, here::here("output", "graft_table.rds"))
 
